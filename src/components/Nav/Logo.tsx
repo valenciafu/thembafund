8622dcf974c64382154a1565f34abbc4cd388ddd
valenciafu <valenciafu@gmail.com>
2020-02-20 17:47:17 -0500

    lint

--- src/components/Nav/Logo.tsx
+++ src/components/Nav/Logo.tsx
@@ -6,16 +6,16 @@ import { HOME_ROUTE } from '../../constants/routes'
 const logoPath = require('../../images/mba-logo.svg') as string // tslint:disable-line
 
 const Image = styled.img<{}>`
-	height: 2rem;
+  height: 2rem;
 
-	margin-bottom: 0;
-	overflow: visible;
-	width: 100%;
-	margin-left: -9vw;
+  margin-bottom: 0;
+  overflow: visible;
+  width: 100%;
+  margin-left: -9vw;
 `
 
 export const Logo = (): React.ReactElement => (
-	<Link to={HOME_ROUTE} style={{ lineHeight: 0 }}>
-		<Image src={logoPath} alt="The MBA Fund" />
-	</Link>
+  <Link to={HOME_ROUTE} style={{ lineHeight: 0 }}>
+    <Image src={logoPath} alt="The MBA Fund" />
+  </Link>
 )
